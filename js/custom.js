2e879230342a538d7d1ebbb3ad9422c45246d858
Khaled <khaled@Khaleds-MBP.home>
2014-11-30 20:22:58 -0500

    first init yay

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    impress().init(); //starts impress.js 
+
+    // Entering State  (step)
+    window.addEventListener('impress:stepenter', function() {
+
+        
+
+    });
+
+    //Leaving state (step)
+    window.addEventListener('impress:stepleave', function() {
+
+       
+    });
+
+    //entering state (substep)
+    window.addEventListener('impress:substep-active', function() {
+       
+
+    });
+
+    //leaving state (substep)
+    window.addEventListener('impress:substep-inactive', function() {
+      
+
+    });
+
+});
