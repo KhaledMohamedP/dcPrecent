b0f87a7f81ce5dce8d4c7f0e94c3891403c460c8
Khaled <khaled@Khaleds-MacBook-Pro.local>
2014-12-07 21:17:44 -0500

    Added the One More thing for big notation

--- js/custom.js
+++ js/custom.js
@@ -15,7 +15,9 @@ $(document).ready(function() {
 
     //entering state (substep)
     window.addEventListener('impress:substep-active', function() {
-       
+
+       $('#oneMoreThing.active').parent().find('h1').removeClass("em7")
+                                        .text("One More Thing...").addClass("em4 animated bounceInDown");
 
     });
 
